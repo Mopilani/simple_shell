acb00a3aa2048a9c4613d89a332ae2046d0ae884
Mohammed Abderrahim <mopilani1@gmail.com>
2023-10-09 04:16:20 -0700

    Add mainh

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef  _MAIN_H_
+#define  _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+
