c1ae84a6db58d1be454fcc542157afddae1ac18b
Mohammed Abderrahim <mopilani1@gmail.com>
2023-10-17 04:50:13 -0700

    U145

---
+++ block/getEnv.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+/**
+ * _get_envir - returns the string array copy of our environ
+ * @info: Structure containing potential arguments.
+ * Return: 0
+ */
+char **_get_envir(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var:  string env var property
+ * @value: the string env var value
+ *  Return:0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buffer = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buffer = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buffer)
+		return (1);
+	_strcpy(buffer, var);
+	_strcat(buffer, "=");
+	_strcat(buffer, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buffer;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buffer, 0);
+	free(buffer);
+	info->env_changed = 1;
+	return (0);
+}
