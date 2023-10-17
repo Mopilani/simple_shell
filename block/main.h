f744c3ac519e39636baedc5a82de9a77d12e2d24
Mohammed Abderrahim <mopilani1@gmail.com>
2023-10-17 04:29:14 -0700

    Tobz

--- block/main.h
+++ block/main.h
@@ -200,4 +200,49 @@ int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
 
 
+/* getinfo.c */
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+
+/* environ.c */
+char *_get_env_val(info_t *, const char *);
+int _menv(info_t *);
+int _new_setenv(info_t *);
+int _rm_unsetenv(info_t *);
+int populate_env_lst(info_t *);
+
+/* getenv.c */
+char **_get_envir(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
+/* history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+
+/* lists.c */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* lists1.c */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+
+/* tovars.c */
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
+
 #endif
