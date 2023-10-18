0a2601459028d58eba405eddf4820bb6abbe54f7
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-18 14:45:17 +0200

    Hot update fo better source view

--- _atoi.c
+++ _atoi.c
@@ -2,26 +2,27 @@
 
 /**
  * _atoi - convert a string into an integer.
- * @s: the string to use.
+ * @str: the string to use.
  * Return: integer.
  */
 
-int _atoi(char *s)
+int _atoi(char *str)
 {
-	int sign = 1, i = 0;
+	int sign = 1;
+	int i = 0;
 
 	unsigned int res = 0;
 
-	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	while (!(str[i] <= '9' && str[i] >= '0') && str[i] != '\0')
 	{
-		if (s[i] == '-')
+		if (str[i] == '-')
 			sign *= -1;
 		i++;
 	}
 
-	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	while (str[i] <= '9' && (str[i] >= '0' && str[i] != '\0'))
 	{
-		res = (res * 10) + (s[i] - '0');
+		res = (res * 10) + (str[i] - '0');
 		i++;
 	}
 
