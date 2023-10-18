8c1e46428952ff0e8de7be2db9d6ea80f5eea450
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-18 15:12:20 +0200

    Change 154

--- SLMDF/main.h
+++ SLMDF/main.h
@@ -39,17 +39,19 @@ extern dou_p environ;
 char *_strcat(string dest, string src);
 int _strcmp(string s1, string s2);
 int _strchr(string s, char c);
+char *_strcpy(string dest, string src);
+char *_strdup(string str);
 
 /* _str1.c */
 char *_strncpy(char *dest, char *src, size_t n);
 int _strncmp(char *str1, char *str2, size_t n);
 char *_strtok(string str, string _delimiters);
-char *_strcpy(string dest, string src);
 int _strlen(string s);
-char *_strdup(string str);
-
 int _print_string(string st);
+
+/* _putchar.c */
 int _putchar(char c);
+
 int _isdigit(int c);
 
 /* _atoi.c */
