abd72ea859f3f0b97363f513731b333199180754
Mohammed Abderrahim <mopilani1@gmail.com>
2023-10-18 05:13:25 -0700

    Hot fix

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
-int main(int ac, char **argv){
-  char *prompt = "(Eshell) $ ";
-  char *lineptr;
-  size_t n = 0; 
+/**
+ * main - entry point
+ * @ac: arg count
+ * @argv: arg vector
+ *
+ * Return:if ( success) 0,else  1 on error
+ */
+int main(int ac, char **argv)
+{
+	char *prompt = "(Eshell) $ ";
+	char *lineptr;
+	size_t n = 0;
 
-  /* declaring void variables */
-  (void)ac; (void)argv;
+	/* declaring void variables */
+	(void)ac;
+	(void)argv;
 
-  printf("%s", prompt);
-  getline(&lineptr, &n, stdin);
-  printf("%s\n", lineptr);
+	printf("%s", prompt);
+	getline(&lineptr, &n, stdin);
+	printf("%s\n", lineptr);
 
-  free(lineptr);
-  return (0);
+	free(lineptr);
+	return (0);
 }
 
--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 
