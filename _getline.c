a22dcceaaef114e8f1d957912e0ae8288bc5ac38
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-10-18 16:18:19 +0200

    HTf 323

--- _getline.c
+++ _getline.c
@@ -9,6 +9,8 @@
  */
 size_t _getline(char **getlineptr, size_t *bffsz, int f_d)
 {
+			printf("Getting line");
+
 	char *buffr = NULL;
 	size_t total_rd = 0;
 	ssize_t rd;
@@ -53,5 +55,6 @@ size_t _getline(char **getlineptr, size_t *bffsz, int f_d)
 	} while (rd > 0 && buffr[total_rd - 1] != '\n');
 
 	buffr[total_rd] = '\0';
+			printf("Line getted");
 	return (total_rd);
 }
