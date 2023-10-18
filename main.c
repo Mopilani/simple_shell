0caad6ad4383f1a9d151edc1b94d048f0f306b1b
Mohammed Abderrahim <mopilani1@gmail.com>
2023-10-18 04:59:42 -0700

    Test 0.1

--- main.c
+++ main.c
@@ -5,9 +5,14 @@ int main(int ac, char **argv){
   char *lineptr;
   size_t n = 0; 
 
+  /* declaring void variables */
+  (void)ac; (void)argv;
+
   printf("%s", prompt);
   getline(&lineptr, &n, stdin);
+  printf("%s\n", lineptr);
 
   free(lineptr);
   return (0);
 }
+
